baacb22d0b4db01c540007380df494516fe7cd0a
Andrey Plotnikov <plo-av@protonmail.com>
2022-11-29 18:06:36 +1000

    added sort_pathlib, modified rename_for_mp3

--- for-audio/rename_for_mp3.py
+++ for-audio/rename_for_mp3.py
@@ -1,7 +1,20 @@
 import os
 
-# переименование файлов mp3 в текущей папке, срезание начала в названии
-for items in os.listdir():
-    if items.endswith('.mp3'):
-        os.rename(items, str(items[4:]))
-        print(items)
+# переименование файлов mp3, flac в текущей папке, срезание начала в названии, удаление повторов
+extension = ['.mp3', '.flac']
+for file in os.listdir():
+    try:
+        if [unit for unit in extension if file.endswith(unit)]:
+            if file.split()[-1] in ['копия' + i for i in extension]:
+                [os.rename(file, file.replace(file.split()[-1], '').replace(file.split()[-2], i).
+                           replace(' ' + i, i)) for i in extension if file.endswith(i)]
+                continue
+            name = file
+            for letter in file:
+                if letter.istitle():
+                    break
+                else:
+                    name = name[1:]
+            os.rename(file, name)
+    except FileExistsError:
+        os.remove(file)
