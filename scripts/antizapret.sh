21b4930bf533122ca369f09e9b172ca17329a180
Andrey <kms.amur@gmail.com>
2024-03-31 19:14:59 +1000

    update

--- scripts/antizapret.sh
+++ scripts/antizapret.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-if printf "\e[35m%s\e[0m" "$(curl -i -s https://antizapret.prostovpn.org:8443/proxy.pac | rg "location:" | cut -f2 -d " ")"; then echo ; fi
+#antizapret.prostovpn.org
+if printf "\e[35m%s\e[0m" "$(curl -i -s https://p.thenewone.lol:8443/proxy.pac | rg "location:" | cut -f2 -d " ")"; then echo ; fi
+
+#also works:
+#sudo -- sh -c "echo '195.123.208.131 antizapret.prostovpn.org' >> /etc/hosts"
+#and
+#curl -i -s https://antizapret.prostovpn.org:8443/proxy.pac | rg "location:" | cut -f2 -d " "
