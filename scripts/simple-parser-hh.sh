003250cd513a901ff9331fd90e6c70b8fb46d0c2
Andrey <kms.amur@gmail.com>
2024-04-02 02:04:12 +1000

    update

--- scripts/simple-parser-hh.sh
+++ scripts/simple-parser-hh.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 WHITE="\033[37m"
 VIOLET="\033[35m"
+BLUE="\033[36m"
+DBLUE="\033[34m"
 YELLOW="\033[1;33m"
 NORMAL="\033[0m"
 time="&order_by=publication_time"
@@ -14,14 +16,15 @@ if [ ${#request} -gt 0 ]
   then
   for page in $(seq 1 100)
   do
-  name=$(printf "%s" "$request" | jq -r ".items.[$page].name"); echo -e "${WHITE}$name${NORMAL}"
+  name=$(printf "%s" "$request" | jq -r ".items.[$page].name"); echo -e "${WHITE}$name${NORMAL}" | sed "s/null/--- конец ---/g"
   if [ "$name" = "null" ]; then break; fi
-  company=$(printf "%s" "$request" | jq -r ".items.[$page].employer.name"); echo -e "${VIOLET}$company${NORMAL}"
-  printf "%s" "$request" | jq -r ".items.[$page].published_at"
+  company=$(printf "%s" "$request" | jq -r ".items.[$page].employer.name"); echo -e "фирма: ${VIOLET}$company${NORMAL}"
+  date=$(printf "%s" "$request" | jq -r ".items.[$page].published_at" | sed "s/+.*//g" | tr "T" " ")
+  echo -e "дата: ${DBLUE}$date${NORMAL}"
   from=$(printf "%s" "$request" | jq -r ".items.[$page].salary.from")
   to=$(printf "%s" "$request" | jq -r ".items.[$page].salary.to")
-  echo "зарплата: $from - $to"
-  alternate_url=$(printf "%s" "$request" | jq -r ".items.[$page].alternate_url"); echo -e "${YELLOW}$alternate_url${NORMAL}"
-  echo
+  echo -e "зарплата: ${BLUE}$from${NORMAL} - ${BLUE}$to${NORMAL}"
+  alternate_url=$(printf "%s" "$request" | jq -r ".items.[$page].alternate_url"); echo -e "ссылка: ${YELLOW}$alternate_url${NORMAL}"
+  echo 
   done
 fi
