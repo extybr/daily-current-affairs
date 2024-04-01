99333e6347f8537f3d07e61ee6e84042f7f1586c
Andrey <kms.amur@gmail.com>
2024-04-01 22:12:21 +1000

    update

--- scripts/my-ip-addr.sh
+++ scripts/my-ip-addr.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ip -c route
-if printf "Your IP address: \e[31m%s\e[0m" "$(curl -s "https://api.ipify.org")"; then printf "\e[37m > https://api.ipify.org\e[0m\n"; fi
-if printf "Your IP address: \e[31m%s\e[0m" "$(curl -s "http://ident.me")"; then printf "\e[37m > http://ident.me\e[0m\n"; fi
-if printf "Your IP address: \e[31m%s\e[0m" "$(curl -s --connect-timeout 2 "https://api.myip.com" | jq -r ".ip")"; then printf "\e[37m > https://api.myip.com\e[0m\n"; fi
-if printf "Your IP address: \e[31m%s\e[0m" "$(curl -s ifconfig.me)"; then printf "\e[37m > ifconfig.me\e[0m\n"; fi
+ident=$(curl -s --max-time 2 "http://ident.me")
+if [ "${#ident}" -gt 0 ]; then printf "Your IP address: \e[31m%s\e[0m \e[37m > http://ident.me\e[0m\n" "$ident"; fi
+myip=$(curl -s --max-time 2 "https://api.myip.com" | jq -r ".ip")
+if [ "${#myip}" -gt 0 ]; then printf "Your IP address: \e[31m%s\e[0m \e[37m > https://api.myip.com\e[0m\n" "$myip"; fi
+ifconfig=$(curl -s --max-time 2 ifconfig.me)
+if [ "${#ifconfig}" -gt 0 ]; then printf "Your IP address: \e[31m%s\e[0m \e[37m > ifconfig.me\e[0m\n" "$ifconfig"; exit 0; fi
