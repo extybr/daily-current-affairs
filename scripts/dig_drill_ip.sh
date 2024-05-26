bbca50f4ac0c00a24ccaf94a9617774253ad550e
Andrey <kms.amur@gmail.com>
2024-05-26 16:59:47 +1000

    update

--- scripts/dig_drill_ip.sh
+++ scripts/dig_drill_ip.sh
@@ -7,7 +7,9 @@ if command -V dig &> /dev/null; then
   ip_addr=$(dig +trace "$1" @8.8.8.8 | grep -E "^$1" | grep 'A' | tr -d '\t' | cut -d 'A' -f 2)
 elif command -V drill &> /dev/null; then
   ip_addr=$(drill "$1" -T @8.8.8.8 | grep -E "^$1" | grep 'A' | tr -d '\t' | cut -d 'A' -f 2)
-else echo -e "command ${blue}dig${normal} and ${blue}drill${normal} not found"
+elif command -V nslookup &> /dev/null; then
+  ip_addr=$(nslookup vk.com | grep 'Address:' | grep -v '192.168.' | cut -d " " -f 2)
+else echo -e "command ${blue}dig${normal}, ${blue}drill${normal} and ${blue}nslookup${normal} not found"
   exit 0
 fi
 echo -e "${blue}${ip_addr}${normal}"
