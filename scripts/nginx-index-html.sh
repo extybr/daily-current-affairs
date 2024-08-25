0f49ccaad8f017e8d2bb98e2dd8a74289ac83b9e
Andrey <kms.amur@gmail.com>
2024-08-25 10:56:08 +1000

    update

---
+++ scripts/nginx-index-html.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+html_path="/usr/share/nginx/html"
+if [ -f "${html_path}"/index.html ]
+  then
+    sudo rm "${html_path}"/index.html
+    sudo cp "${html_path}"/index.basic.html "${html_path}"/index.html
+    echo 'file `index.html` copied'
+  else 'file `index.html` missing'
+fi
