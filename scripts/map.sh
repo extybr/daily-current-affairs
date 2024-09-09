756732e2fc10f4649d4d39432630964ebcb5141f
Andrey <kms.amur@gmail.com>
2024-09-10 01:13:11 +1000

    update

--- scripts/map.sh
+++ scripts/map.sh
@@ -1,9 +1,50 @@
 #!/bin/sh
 
-cd ~/PycharmProjects/temp/map_with_customtkinter
-if [ "$#" -gt 1 ]
-  then echo "ожидалось не более 1 параметра, а передано $#"
-elif [ "$#" -eq 1 ]
-  then venv/bin/python main.py "$1"
-else venv/bin/python main.py
+white="\e[37m"
+violet="\e[35m"
+blue="\e[36m"
+dblue="\e[34m"
+yellow="\e[33m"
+normal="\e[0m"
+red="\e[31m"
+
+file_map='https://github.com/TomSchimansky/TkinterMapView/blob/main/examples/map_with_customtkinter.py'
+requirements='https://github.com/TomSchimansky/TkinterMapView/blob/main/requirements.txt'
+if ! [[ $(ls ~/PycharmProjects/temp/map_with_customtkinter 2> /dev/null) ]]
+then echo -e "${red}not found${normal} folder <${white}map_with_customtkinter${normal}>"
+echo -e "${blue}wget${normal} ${file_map}\n${blue}wget${normal} ${requirements}"
+echo -e "'${white}pip install -r requirements.txt${normal}' or '${white}pip install tkintermapview${normal}'"
+echo -e "${blue}mv${normal} ${violet}map_with_customtkinter.py${normal} ${dblue}main.py${normal}"
+echo -e "${blue}sed${normal} -i ${yellow}'6i\CITY = 'New-York''${normal} ${dblue}main.py${normal}"
+echo -e "${blue}sed${normal} -i ${yellow}'7i\if len(sys.argv) == 2:'${normal} ${dblue}main.py${normal}"
+echo -e "${blue}sed${normal} -i ${yellow}'8i\    CITY = sys.argv[1]'${normal} ${dblue}main.py${normal}"
+exit 0
 fi
+
+cd ~/PycharmProjects/temp/map_with_customtkinter
+
+map() {
+venv/bin/python main.py
+}
+
+fix() {
+sed -i '347i\\n' "${osm}"
+sed -i '348i\    def _build_headers(self, provider_key, **kwargs):' "${osm}"
+sed -i '349i\        """Will be overridden according to the targetted web service"""' "${osm}"
+sed -i '350i\        return {"User-Agent": "My User Agent 1.0"}' "${osm}"
+}
+
+pwd=$(pwd)
+python_version=$(ls ${pwd}/venv/lib)
+osm="${pwd}/venv/lib/${python_version}/site-packages/geocoder/osm.py"
+if [[ $(grep '_build_headers' "${osm}") ]]
+then
+  if [ "$#" -gt 1 ]
+    then echo -e "${white}ожидалось не более 1 параметра, а передано $#${normal}"
+  elif [ "$#" -eq 1 ]
+    then venv/bin/python main.py "$1"
+  else map
+  fi
+else echo -e "${white}*** fix ***${normal}" && fix && map
+fi
+
--- scripts/timestamp.sh
+++ scripts/timestamp.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
+white="\e[37m"
+blue="\e[36m"
+yellow="\e[33m"
+normal="\e[0m"
+red="\e[31m"
+
 errors() {
-echo -e "The data format is invalid.\nExample:"
-echo "convert timestamp to date: '1725787421'"
-echo "convert date to timestamp: 'm/d/y H:M:S'"
+echo -e "${red}The data format is invalid.${normal}\n${yellow}Example:"
+echo -e "convert timestamp to date:${normal} '${blue}1725787421${normal}'"
+echo -e "${yellow}convert date to timestamp:${normal} '${blue}m/d/y H:M:S${normal}'"
 }
 
 if [ "$#" -eq 0 ]
-  then date +%s  # unix timestamp
+  then current_ts=$(date +%s)  # unix timestamp
+  echo -e "${blue}${current_ts}${normal}"
 elif [ "$#" -eq 1 ]
   then 
   if [ "$1" -gt 0 ] 2>/dev/null
     then ts=$(date --date="@$1" 2> /dev/null)
     if ! [[ "${ts}" ]]
       then errors
-      else echo "${ts}"
+      else echo -e "${blue}${ts}${normal}"
     fi
   else dt=$(date -d "$1" +"%s" 2> /dev/null)
     if ! [[ "${dt}" ]]
       then errors
-      else echo "${dt}"
+      else echo -e "${blue}${dt}${normal}"
     fi
   fi
-else echo "ожидалось не более 1 параметра, а передано $#"
+else echo -e "${white}ожидалось не более 1 параметра, а передано $#${normal}"
 fi
