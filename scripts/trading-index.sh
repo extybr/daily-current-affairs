9985bc508c05bd6af23e55b69ba22d7a3a86a7d7
Andrey <kms.amur@gmail.com>
2024-09-18 16:28:57 +1000

    update

--- scripts/trading-index.sh
+++ scripts/trading-index.sh
@@ -1,19 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-get() {
+violet="\e[35m"
+blue="\e[36m"
+yellow="\e[33m"
+normal="\e[0m"
+
+request() {
 result=$(curl -s 'https://ru.tradingview.com/markets/indices/quotes-major/' | grep -oP '{"s":"SP:SPX"[^>]+"tot' | sed 's/],"tot//g ; s/},{"s"/}{"s"/g')
 IFS=$'\n'
 for line in "${result}"
 do 
-index=$(echo $line | jq -r '.s')
-title=$(echo $line | jq -r '.d[1]')
-value=$(echo $line | jq -r '.d[6]')
-paste -d ' | ' <(echo "${index}") <(echo "${title}") <(echo "${value}")
+index=($(echo $line | jq -r '.s'))
+title=($(echo $line | jq -r '.d[1]'))
+value=($(echo $line | jq -r '.d[6]'))
+
 done
 }
 
-IFS=$' | '
-echo $(get) | while read item; do
-echo -e "[*] ${item}"
-done
+request
 
+for number in {0..24}; do
+echo -e "${blue}${index[${number}]}${normal}  ${violet}${title[${number}]}${normal}  ${yellow}${value[${number}]}${normal}"
+done
