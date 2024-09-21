bfc68cc1ba6bf4e0a87a55234a0d7c07e2dab6c0
Andrey <kms.amur@gmail.com>
2024-09-21 19:34:12 +1000

    update

--- scripts/pornolab.py
+++ scripts/pornolab.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 ###########################
-# $> ./rutracker.py       #
-# $> ./rutracker.py  252  #
-# $> ./rutracker.py  -h   #
+# $> ./pornolab.py        #
+# $> ./pornolab.py 1867   #
+# $> ./pornolab.py -h     #
 ###########################
 
 import sys
@@ -15,7 +15,7 @@ blue = "\033[36m"
 normal = "\033[0m"
 
 cmd = "curl -s --proxy n.thenewone.lol:29976 --max-time 10 --location "
-number = 252
+number = 1867
 
 
 def fail() -> None:
@@ -25,24 +25,24 @@ def fail() -> None:
 
 if len(sys.argv) == 2:
     if sys.argv[1] in ('h', '-h', 'help', '-help'):
-        print(' 252 - Фильмы 2024 года\n 934 - Азиатские фильмы\n'
-              ' 505 - Индийское кино\n 1803 - Новинки и сериалы в '
-              'стадии показа (HD Video)')
+        print(" 1867 - Сайтрипы 2024 (HD Video) / SiteRip's 2024 (HD Video)\n"
+              " 883 - Эротические студии Разное / Erotic Picture Gallery "
+              "(various)\n 1726 - MetArt & MetModels")
         exit(0)
     else:
         number = sys.argv[1] if sys.argv[1].isdigit() else fail()
 
-link = f"'https://rutracker.org/forum/viewforum.php?f={number}'"
+link = f"'https://pornolab.net/forum/viewforum.php?f={number}'"
 html = subprocess.getoutput(cmd + link, encoding='cp1251')
 
 if not html:
     fail()
 
-pattern = r'tt-\d{7}.+</a>'
+pattern = r'.+tt-text.+</a>'
 
 result = re.compile(pattern).findall(html)
 
 for item in result:
-    url = 'https://rutracker.org/forum/' + item[18:41]
-    title = item[73:-4].replace('<wbr>', '')
+    url = 'https://pornolab.net/forum/' + item[13:36]
+    title = item[65:-4].replace('<wbr>', '')
     print(f'{blue}{title}{normal}\n{yellow}{url}{normal}\n')
