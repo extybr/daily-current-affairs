56e11fd77c549036f1dc866bae0881f7565a218f
Andrey <kms.amur@gmail.com>
2024-09-22 08:01:44 +1000

    update

--- scripts/element_in_folder.sh
+++ scripts/element_in_folder.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+#################################
+# $> ./element_in_folder.sh ~   #
+#################################
+
+if [ "$#" -ne 1 ]
+  then folder="${HOME}"
+else folder="$1"
+fi
+
 IFS=$'\n'
-for item in ${HOME}/*
+for item in ${folder}/*
 do
-basename "${item}"
-if [ -d ${item} ]; then echo "${item} is folder"
-elif [ -f ${item} ]; then echo "${item} is file"; fi
-echo
+  basename "${item}"
+  if [ -d ${item} ]
+    then echo "${item} is folder"
+  elif [ -f "${item}" ]
+    then echo "${item} is file"
+  fi
+  echo
 done
+
--- scripts/local_server_forward_serveo.sh
+++ scripts/local_server_forward_serveo.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+#########################################
+# $> ./local_server_forward_serveo.sh   #
+#########################################
+
+port=9090
 terminal="terminator -g /home/tux/.config/terminator/config --new-tab -e"
-${terminal} "python -m http.server 9090; zsh"
-${terminal} "ssh -R 80:localhost:9090 serveo.net"
+${terminal} "python -m http.server ${port}; zsh"
+${terminal} "ssh -R 80:localhost:${port} serveo.net"
 # ssh -R my-special-subdomain:80:localhost:9090 serveo.net
 echo "Смотрите ссылку во вкладке терминала"
 
