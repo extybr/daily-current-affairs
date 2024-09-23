da26d232bad0b79328913f2ef6c03674b1d971f3
Andrey <kms.amur@gmail.com>
2024-09-23 21:59:54 +1000

    update

--- scripts/os.sh
+++ scripts/os.sh
@@ -5,6 +5,6 @@ hostnamectl
 echo
 inxi -c11 -F 2> /dev/null
 printf "\n%s\n\n" "$(pacman-mirrors)"
-neofetch 2> /dev/null
+fastfetch -c all.jsonc 2> /dev/null
 printf "Your IP address: \e[31m%s\e[0m  (time: %s)\n" "$(curl -s --max-time 3 ifconfig.me)" "$(date '+%d-%m-%y %T')"
 uname -a
