4904b14bf23b9cd02184448b1b704a0eab1846f4
Andrey <kms.amur@gmail.com>
2024-09-23 12:01:55 +1000

    update

--- scripts/current_time_area_google.sh
+++ scripts/current_time_area_google.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# ./current_time_area_google.sh New-York
+############################################
+# ./current_time_area_google.sh New-York   #
+############################################
+
 user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
 time=$(curl -s -A "${user_agent}" "https://www.google.com/search?q=current+time+$1" | \
 grep -oP '"3" role="heading">[^<]+' | sed "s/\"3\" role=\"heading\">/\\n/g")
-echo -e "\e[31m${time}\e[0m\\n"
-curl -i --silent 'https://google.com' 2>&1 | grep date  # date (GMT) from google
+echo -e "\e[31m${time}\e[0m\n"
+curl -I 'https://google.com' 2>&1 | grep date  # date (GMT) from google
+
--- scripts/dht-btdig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-title=$(echo "$1" | sed 's/ /+/g')
-curl -s --max-time 10 "https://btdig.com/search?order=0&q=${title}" | grep -oP 'magnet[^>]+" t' | sed 's/" t//g'
