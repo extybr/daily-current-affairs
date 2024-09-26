c2aeb8e806bce9bc3da2ba6b9759c16c8fcbde07
Andrey <kms.amur@gmail.com>
2024-09-27 02:13:35 +1000

    update

--- scripts/bitsearch.sh
+++ scripts/bitsearch.sh
@@ -25,12 +25,15 @@ if ! [[ "${html}" ]]; then request; fi
 
 request
 magnets=($(echo "${html}" | grep -oP 'magnet:?[^>]+" data' | sed 's/" data//g'))
-links=($(echo "${html}" | grep -oP '<a href="/torrents/[^>]+" data' | sed 's/<a href="/https:\/\/bitsearch.to/g ; s/" data//g'))
+links=($(echo "${html}" | grep -oP '<a href="/torrents/[^>]+" data' | \
+         sed 's/<a href="/https:\/\/bitsearch.to/g ; s/" data//g'))
 word='%5BBitsearch.to%5D'
 
 uri() {
-#echo "$1" | sed "s/+/ /g ; s/%C2%AE/®/g ; s/%20/ /g ; s/%21/\!/g ; s/%22/\"/g ; s/%23/#/g ; s/%24/$/g ; s/%25/%/g ; s/%26/&/g ; s/%27/'/g ; s/%28/(/g ; s/%29/)/g ; s/%2A/*/g ; s/%2B/+/g ; s/%2C/,/g ; s/%2F/\//g ; s/%3A/:/g ; s/%3B/;/g ; s/%3D/=/g ; s/%3F/?/g ; s/%40/@/g ; s/%5B/[/g ; s/%5D/]/g ; s/&#x3D;/=/g ; s/amp;//g"
-
+#echo "$1" | sed "s/+/ /g ; s/%C2%AE/®/g ; s/%20/ /g ; s/%21/\!/g ; s/%22/\"/g ; s/%23/#/g ; \
+#s/%24/$/g ; s/%25/%/g ; s/%26/&/g ; s/%27/'/g ; s/%28/(/g ; s/%29/)/g ; s/%2A/*/g ; s/%2B/+/g ; \
+#s/%2C/,/g ; s/%2F/\//g ; s/%3A/:/g ; s/%3B/;/g ; s/%3D/=/g ; s/%3F/?/g ; s/%40/@/g ; s/%5B/[/g ; \
+#s/%5D/]/g ; s/&#x3D;/=/g ; s/amp;//g"
 echo $(./url_decoder.py "$1") | sed 's/&#x3D;/=/g ; s/amp;//g ; s/+/ /g'
 }
 
