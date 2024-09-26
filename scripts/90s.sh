2438ccbef374f23280a9c38fb453c86042db72af
Andrey <kms.amur@gmail.com>
2024-09-27 02:38:25 +1000

    update

--- scripts/90s.sh
+++ scripts/90s.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 source ~/PycharmProjects/github/daily-current-affairs/scripts/set_get_volume.sh
-ffplay 'https://regiocast.streamabc.net/regc-90s90spop4760822-mp3-192-9403761?sABC=6516so71%230%239n07p0q896s641n1s7p39465rn25rq94%23vgharf&aw_0_1st.playerid=itunes&amsparams=playerid:itunes;skey:1696004977' -nodisp
+ffplay "https://regiocast.streamabc.net/regc-90s90spop4760822-mp3-192-940376\
+1?sABC=6516so71%230%239n07p0q896s641n1s7p39465rn25rq94%23vgharf&aw_0_1st.play\
+erid=itunes&amsparams=playerid:itunes;skey:1696004977" -nodisp
 clear
