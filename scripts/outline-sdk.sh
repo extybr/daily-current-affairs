88fb6da63bc80b35579fa79ace21ae0dbf74a605
Andrey <kms.amur@gmail.com>
2024-09-27 16:35:56 +1000

    update

---
+++ scripts/outline-sdk.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#https://github.com/Jigsaw-Code/outline-sdk
+
+port_8080() {
+# port=8080
+go run github.com/Jigsaw-Code/outline-sdk/x/examples/http2transport@latest \
+-transport "${outline_key}" -localAddr localhost:8080
+}
+
+port_1080() {
+# port=1080
+go run github.com/Jigsaw-Code/outline-sdk/x/examples/http2transport@latest \
+-transport "${outline_key}"
+}
+
+transport() {
+# port=1080
+./http2transport -transport "${outline_key}"
+}
+
+if ! [ -f ~/.config/autostart/outline-sdk.desktop ]; then
+  echo "[Desktop Entry]
+Type=Application
+Name=outline-sdk
+Comment=outline-sdk script
+Path=${HOME}/PycharmProjects/github/daily-current-affairs/scripts
+Exec=outline-sdk
+StartupNotify=false
+Terminal=false" > outline-sdk.desktop
+  mv outline-sdk.desktop ~/.config/autostart
+fi
+
+if ! [ -f ~/PycharmProjects/github/daily-current-affairs/scripts/outline-sdk ]; then
+  cd ~/PycharmProjects/github/daily-current-affairs/scripts
+  shc -r -f outline-sdk.sh
+  rm outline-sdk.sh.x.c
+  mv outline-sdk.sh.x outline-sdk
+  chmod u=rwx,g=r,o=r outline-sdk
+fi
+
+if [ -f ~/my_programs/outline-sdk/http2transport ]; then
+  cd ~/my_programs/outline-sdk
+  source outline.key
+  transport &
+elif [ -d ~/my_programs/outline-sdk ]; then
+  cd ~/my_programs/outline-sdk
+  source outline.key
+  port_8080 &
+else exit 0
+fi
+
