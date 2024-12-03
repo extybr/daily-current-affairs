0d5565465954c9ac5a72e4a944cbaf720a264e9d
Andrey <kms.amur@gmail.com>
2024-12-04 01:05:04 +1000

    update

--- scripts/python_release.sh
+++ scripts/python_release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# $> ./python_release.sh
 
 blue="\e[36m"
 normal="\e[0m"
@@ -9,6 +10,7 @@ IFS=$'\n'
 html=$(curl -s 'https://www.python.org/downloads/feed.rss')
 date=($(echo "${html}" | grep -oP 'Release date:[^<]+' | sed 's/+00:00//g'))
 version=($(echo "${html}" | grep -oP '<link>[^<]+' | tail +2 | sed 's/<link>//g'))
-for i in {0..9}; do
-  echo -e "${date[$i]}\n${blue}${version[$i]}${normal}"
+for number in {0..9}; do
+  echo -e "${date[$number]}\n${blue}${version[$number]}${normal}"
 done
+
--- scripts/selezen_top_week.sh
+++ scripts/selezen_top_week.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# $> ./selezen_top_week.sh
 
 blue="\e[36m"
+purple="\e[35m"
 normal="\e[0m"
 
 source ./proxy.sh 1> /dev/null
 
-url='https://use.selezen.club/best-of-the-week.html'
+src='https://use.selezen.club/best-of-the-week.html'
 tag1='<h6 class=\"mb-1 font-14\">'
 tag2='<tr role=\"row\" class=\"cursor-pointer\" onclick=\"location.href='
-top_week=$(curl -s ${proxy} ${url} | \
-           grep -oP "(${tag1}[^<]+|${tag2}[^\;\"]+)" | \
-           sed "s/${tag1}//g ; s/${tag2}//g")
 
-echo -e "${blue}${top_week}${normal}"
+html=$(curl -s ${proxy} ${src})
+name=$(echo "${html}" | grep -oP "${tag1}[^<]+" | sed "s/${tag1}//g")
+url=($(echo "${html}" | grep -oP "${tag2}[^<]+" | sed "s/${tag2}//g ; s/;\">//g ; s/html'/html/g ; s/'https/https/g"))
+
+IFS=$'\n'
+counter=0
+for title in ${name}; do
+  echo -e "${purple}${title}${normal}"
+  echo -e "${blue}${url[$counter]}${normal}\n"
+  ((counter+=1))
+done
 
