6ef1e63e19d96e8a8356ab1b2d1a598a8fba532f
Andrey <kms.amur@gmail.com>
2024-12-05 09:54:37 +1000

    update

--- scripts/megapeer.sh
+++ scripts/megapeer.sh
@@ -9,11 +9,11 @@ source ./proxy.sh 1> /dev/null
 
 src='https://megapeer.vip/top'
 tag1='<a href=\"\/torrent'
-tag2='class=\"url\">'
+tag2='\" class'
 tag3='https:\/\/megapeer.vip\/torrent'
 
 html=$(curl -s ${proxy} ${src})
-url=($(echo "${html}" | grep -oP "${tag1}[^<]+" | sed "s/${tag1}/${tag3}/g ; s/${tag2}/\\n/g ; s/\" //g"))
+url=($(echo "${html}" | grep -oP "${tag1}[^<]+${tag2}" | sed "s/${tag1}/${tag3}/g ; s/${tag2}/\\n/g"))
 
 echo -e "${top}ТОП за последние 24 часа${normal}"
 for i in {0..29}; do
