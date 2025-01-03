b08fa417bc59ab44129dd97a3cb2fc5134beeda5
Andrey <kms.amur@gmail.com>
2025-01-03 13:51:50 +1000

    update

---
+++ scripts/photo_from_camera.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 1
+dt=$(date "+%d-%m-%y_%H-%M-%S")
+folder="Изображения"
+file="${HOME}/${folder}/${dt}.png"
+ffmpeg -y -t 1 -f video4linux2 -s 1920x1080 -i /dev/video0 -frames:v 1 -f image2 "${file}" &> /dev/null
+echo -e "\e[036mФото сохранено: \e[37m${file}\e[0m"
+
