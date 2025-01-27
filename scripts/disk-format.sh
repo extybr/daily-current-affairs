adc966f599d95f666483ed98fe76860282045e26
Andrey <kms.amur@gmail.com>
2025-01-27 17:31:49 +1000

    update

--- scripts/disk-format.sh
+++ scripts/disk-format.sh
@@ -11,6 +11,12 @@ function choice {
   echo "${output}"
 }
 
+function empty {
+  if [ -z "$1" ]; then
+    exit 0
+  fi
+}
+
 # выбор утилиты для запуска
 utils=''
 for util in gparted gnome-disks mkfs; do
@@ -19,9 +25,10 @@ for util in gparted gnome-disks mkfs; do
   fi
 done
 echo -e "${utils}" | nl
-echo -e "${blue}Выберите нужное действие:${normal}"
-echo -ne "${white}Нажмите цифру:${normal} "
+echo -e "${CYAN}Выберите нужное действие:${NORMAL}"
+echo -ne "${WHITE}Нажмите цифру:${NORMAL} "
 read number
+empty "${number}"
 action=$(choice "${utils}" "${number}")
 
 if ! [[ "${action}" ]]; then
@@ -34,11 +41,12 @@ if [ "${action}" != 'mkfs' ]; then
 fi
 
 # выбор диска / раздела диска
-echo -e "\n${blue}Выберите диск${normal}"
+echo -e "\n${CYAN}Выберите диск${NORMAL}"
 # ls /dev/sd* | nl  # вывод дисков
 lsblk -o NAME,SIZE,TYPE,MOUNTPOINTS | grep 'sd' | nl
-echo -ne "${white}Нажмите цифру:${normal} "
+echo -ne "${WHITE}Нажмите цифру:${NORMAL} "
 read number
+empty "${number}"
 cmd=$(lsblk | grep 'sd' | \
       sed 's/^\([^ \t]*\).*/\1/ ; s/└─//g ; s/├─//g')
 disk=$(choice "${cmd}" "${number}")
@@ -47,19 +55,20 @@ if ! [[ "${disk}" ]]; then
 fi
 
 # выбор файловой системы
-echo -e "\n${blue}Выберите в какую файловую систему"\
-        "отформатировать диск?${normal}"
+echo -e "\n${CYAN}Выберите в какую файловую систему"\
+        "отформатировать диск?${NORMAL}"
 type_fs="ext4\nntfs\nvfat"
 echo -e "${type_fs}" | nl
-echo -ne "${white}Нажмите цифру:${normal} "
+echo -ne "${WHITE}Нажмите цифру:${NORMAL} "
 read number
+empty "${number}"
 fs=$(choice "${type_fs}" "${number}")
 if ! [[ "${fs}" ]]; then
   exit 0
 fi
 
 # выбор метки диска
-echo -ne "\n${white}Название метки диска:${normal} "
+echo -ne "\n${WHITE}Название метки диска:${NORMAL} "
 read label
 
 # функция форматирования
@@ -75,25 +84,30 @@ function format {
 }
 
 # вывод результата выбора
-echo -e "\n${violet}Выбрано:\n Диск: ${yellow}${disk}\n"\
-        "${violet}Файловая система: ${yellow}${fs}\n"\
-        "${violet}Метка диска: ${yellow}${label}${normal}"
+echo -e "\n${MAGENTA}Выбрано:\n Диск: ${YELLOW}${disk}\n"\
+        "${MAGENTA}Файловая система: ${YELLOW}${fs}\n"\
+        "${MAGENTA}Метка диска: ${YELLOW}${label}${NORMAL}"
+
+if command -V zenity &>/dev/null && ! zenity --question --text="Продолжить?"; then
+  exit
+fi
 
 # подтверждение, отмена или запуск форматирования
-echo -ne "\n${red}Диск будет отмонтирован, данные при"\
-         "форматировании будут удалены.\n${white}Начать"\
-         "форматирование? д/н (y/n)${normal}  "
+echo -ne "\n${RED}Диск будет отмонтирован, данные при"\
+         "форматировании будут удалены.\n${WHITE}Начать"\
+         "форматирование? д/н (y/n)${NORMAL}  "
 read answer
 case "${answer}" in
   [yд])
   format "${disk}" "${fs}" "${label}"
+  notify-send "Сообщение" "Диск отформатирован: успешно" --app-name="FormatDisk" -u critical 2>/dev/null
   ;;
   [nн])
-  echo -e "${white}Отмена${normal}"
+  echo -e "${WHITE}Отмена${NORMAL}"
   exit 0
   ;;
   *)
-  echo -e "${white}Действие не подтверждено${normal}"
+  echo -e "${WHITE}Действие не подтверждено${NORMAL}"
   ;;
 esac
 
--- scripts/text-color.sh
+++ scripts/text-color.sh
@@ -2,11 +2,39 @@
 # используется в других скриптах
 # раскраска текста
 
-normal="\033[0m"
-red="\033[1;31m"
-yellow="\033[33m"
-dblue="\033[34m"
-violet="\033[35m"
-blue="\033[36m"
-white="\033[37m"
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+#######################
+NORMAL="\033[0m"
+BOLD="\033[1m"
+SHADOW="\033[2m"
+ITALIC="\033[3m"
+UNDERLINED="\033[4m"
+BLINK="\033[5m"
+BORDERED="\033[7m"
+STRIKETHROUGH="\033[9m"
+#######################
+RED_BOLD="\033[1;31m"
+GREEN_BOLD="\033[1;32m"
+YELLOW_BOLD="\033[1;33m"
+RED_BOLD_YELLOW="\033[1;31;103m"
+RED_BLINK_YELLOW="\033[5;31;1;103m"
+BLUE_BOLD="\033[1;34m"
+BLUE_BOLD_ITALIC="\033[1;34m\033[3m"
+LIGHT_MAGENTA_BOLD="\033[1;95m"
+LIGHT_CYAN_BOLD="\033[1;96m"
+WHITE_BOLD="\033[1;37m"
+GRAY_BOLD="\033[1;90m"
+#######################
+T_BOLD=$(tput bold)
+T_NORMAL=$(tput sgr0)
+T_BLINK=$(tput blink)
+T_BEL=$(tput bel)
+T_CLEAR=$(tput clear)
 
