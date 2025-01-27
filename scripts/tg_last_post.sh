be90adb43cd32cb418caa07ac5fc14254cc3740d
Andrey <kms.amur@gmail.com>
2025-01-27 23:22:28 +1000

    update

--- scripts/tg_last_post.sh
+++ scripts/tg_last_post.sh
@@ -17,7 +17,7 @@ tg_channel=${name#*"https://t.me/"}
 tg_channel=${name#*"t.me/"}
 
 proxy=''
-source ${SCRIPTS_DIRECTORY}/proxy.sh 1> /dev/null
+source "${SCRIPTS_DIRECTORY/'~'/$HOME}"/proxy.sh 1> /dev/null
 
 html_content_page=$(curl -s ${proxy} "https://t.me/s/${tg_channel}/" | \
   grep  '<span class="tgme_widget_message_views">')
