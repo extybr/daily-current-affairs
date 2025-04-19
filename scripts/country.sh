11c8201798d7c623e4df50f907ace91686b91962
Andrey <kms.amur@gmail.com>
2025-04-19 21:19:06 +1000

    update

--- scripts/country.sh
+++ scripts/country.sh
@@ -5,28 +5,27 @@
 #######################
 # Запуск радиостанции или плейлиста по выбору
 
-source ./set_get_volume.sh
-
-tcn_url=${PLAYLIST_DIRECTORY}"/tcn-live.m3u"
+music_url=${PLAYLIST_DIRECTORY}"/music.m3u"
 radio_country="http://prmstrm.1.fm:8000"
 
 case "$1" in
 1)
-ffplay "${radio_country}/acountry" -nodisp
+ffplay "${radio_country}/acountry" -volume 3 -nodisp
 ;;
 2)
-ffplay "${radio_country}/ccountry" -nodisp
+ffplay "${radio_country}/ccountry" -volume 3 -nodisp
 ;;
 3)
-ffplay "${radio_country}/country" -nodisp
+ffplay "${radio_country}/country" -volume 3 -nodisp
 ;;
 v)
-vlc "${tcn_url%"tcn-live.m3u"*}music.m3u"
+vlc "${music_url}"
 ;;
 s)
-smplayer "${tcn_url%"tcn-live.m3u"*}music.m3u"
+smplayer "${music_url}"
 ;;
 *)
+source ./set_get_volume.sh
 ffplay 'https://strm112.1.fm/country_mobile_mp3?aw_0_req.gdpr=true' -nodisp
 ;;
 esac
