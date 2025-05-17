f2aeee0f0f4ef55649fefcca2a8634dc46359ba8
Andrey <kms.amur@gmail.com>
2025-05-17 23:13:34 +1000

    update

--- scripts/bestchange.sh
+++ scripts/bestchange.sh
@@ -8,7 +8,14 @@
 proxy=''
 source "${SCRIPTS_DIRECTORY/'~'/$HOME}"/proxy.sh &> /dev/null
 
-user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0'
-curl -s ${proxy} -A "${user_agent}" 'https://www.bestchange.ru/sberbank-to-usd-coin.html' | \
-grep -oP 'class="fs">[^<]+' | sed 's/^...........//'
+user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0'
+request=$(curl -s ${proxy} -A "${user_agent}" 'https://www.bestchange.ru/sberbank-to-usd-coin.html' | iconv -f windows-1251 -t utf-8)
+
+company=$(echo "${request}" | grep -oP 'div class="ca" translate="no">\K[^<]+')
+price=$(echo "${request}" | grep -oP 'class="fs">\K[^<]+')
+
+paste <(printf "%s\n" "${company}") <(printf "%s\n" "${price}") |
+while IFS=$'\t' read -r c p; do
+  printf "%s\t\033[35m%s\033[0m\n" "$c" "$p"
+done | nl
 
