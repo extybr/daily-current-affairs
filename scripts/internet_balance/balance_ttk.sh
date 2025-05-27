d4ee57afd164df9ae5f105734e6dc37bf42efe55
Andrey <kms.amur@gmail.com>
2025-05-27 13:20:44 +1000

    update

--- scripts/internet_balance/balance_ttk.sh
+++ scripts/internet_balance/balance_ttk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# $> ./balance_ttk.sh
+# $> ./balance_ttk.sh    # вывод данных
+# $> ./balance_ttk.sh 1  # вывод данных с историей оплаты
 # lk.ttk.ru | Баланс ТТК
 
 source secret.txt  # содержит LOGIN и PASSWORD
@@ -8,63 +9,78 @@ URL="https://lk.ttk.ru"
 COOKIE_FILE="cookie.txt"
 
 function session() {
+
+  # --- Цвета ---
+  
+  red='\033[1;31m'
+  green='\033[1;32m'
+  norm='\033[0m'
+  magenta='\033[35m'
+  cyan='\033[36m'
+
+  # --- Параметры curl ---
+  
+  COMMON_HEADERS=(
+  -A "User-Agent: $USER_AGENT"
+  -H 'Accept: */*'
+  -H 'Accept-Language: ru,en-US;q=0.7,en;q=0.3'
+  -H 'Accept-Encoding: gzip, deflate, br, zstd'
+  -H 'Content-Type: application/json'
+  -H "Origin: $URL"
+  -H 'Connection: keep-alive'
+  -H 'Sec-Fetch-Dest: empty'
+  -H 'Sec-Fetch-Mode: cors'
+  -H 'Sec-Fetch-Site: same-origin'
+  -H 'Sec-GPC: 1'
+  -H 'Priority: u=4'
+  -H 'Pragma: no-cache'
+  -H 'Cache-Control: no-cache'
+)
+
   # --- Получаем новые куки и основные данные сессии ---
 
-  raw=$(curl -s "$URL/api/auth/loginByAccount" -A "User-Agent: $USER_AGENT" \
-  -c "$COOKIE_FILE" -b "$COOKIE_FILE" --fail -H 'Accept: */*' \
-  -H 'Accept-Language: ru,en-US;q=0.7,en;q=0.3' \
-  -H 'Accept-Encoding: gzip, deflate, br, zstd' \
-  -H "Referer: $URL/auth" -H 'Content-Type: application/json' \
-  -H "Origin: $URL" -H 'Connection: keep-alive' -H 'Sec-Fetch-Dest: empty' \
-  -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' \
-  -H 'Sec-GPC: 1' -H 'Priority: u=0' -H 'Pragma: no-cache' \
-  -H 'Cache-Control: no-cache' \
+  raw=$(curl -s --fail "$URL/api/auth/loginByAccount" \
+  -c "$COOKIE_FILE" -b "$COOKIE_FILE" \
+  -H "Referer: $URL/auth" "${COMMON_HEADERS[@]}" \
   --data-raw "{\"login\":\"$LOGIN\",\"password\":\"$PASSWORD\",\"remember\":false}" \
   -c "$COOKIE_FILE" | jq '.[]' 2> /dev/null) || (echo "Ошибка получения страницы логина" && exit)
   
   # --- Вывод данных ---
-
-  contract=$(echo "$raw" | jq -r ".contract")
-  stats=$(echo "$raw" | jq -r ".status")
-  contract_id=$(echo "$raw" | jq -r ".contract_id")
+  
+  contract=$(jq -r '.contract' <<< "$raw")
+  status=$(jq -r '.status' <<< "$raw")
+  contract_id=$(jq -r '.contract_id' <<< "$raw")
 
   echo "contract: $contract"
-  if [ "$stats" = 'Активен' ]; then
-    echo -e "status: \033[1;32m$stats\033[0m"
-  else echo -e "status: \033[1;31m$stats\033[0m"
+  if [ "$status" = 'Активен' ]; then
+    echo -e "status: ${green}${status}${norm}"
+  else
+    echo -e "status: ${red}${status}${norm}"
   fi
   echo "contract_id: $contract_id"
 
-  loginKey=$(grep 'loginKey' "$COOKIE_FILE" 2> /dev/null | awk '{print $NF}')
-  sessionId=$(grep 'sessionId' "$COOKIE_FILE" 2> /dev/null | awk '{print $NF}')
+  loginKey=$(awk '/loginKey/ {print $NF}' "$COOKIE_FILE" 2>/dev/null)
+  sessionId=$(awk '/sessionId/ {print $NF}' "$COOKIE_FILE" 2>/dev/null)
 
   echo "loginKey: $loginKey"
   echo -e "sessionId: $sessionId\n"
   
-  counter=0
-  for var in "${loginKey}" "${sessionId}"; do
-    if [ "$counter" -eq 2 ]; then
-      echo -e "Не получены \033[31mloginKey,sessionId\033[0m" && exit
-    fi
-    if [ -z "$var" ]; then
-      echo -e "Не получены \033[31mloginKey,sessionId\033[0m" 
+  for attempt in {1..2}; do
+    if [[ -z "$loginKey" || -z "$sessionId" ]]; then
+      echo -e "Не получены ${red}loginKey, sessionId${norm}"
+      (( attempt == 2 )) && exit
       sleep 2
-      counter=$[ counter + 1 ]
       session
+    else
+      break
     fi
   done
-  
+
   # --- Первый POST запрос данных в json-формате ---
 
-  info=$(curl -s --fail "$URL/api/user" -A "User-Agent: $USER_AGENT" -H 'Accept: */*' \
-  -H 'Accept-Language: ru,en-US;q=0.7,en;q=0.3' \
-  -H 'Accept-Encoding: gzip, deflate, br, zstd' \
-  -H "Referer: $URL/" -H 'Content-Type: application/json' \
-  -H "Origin: $URL" -H 'Connection: keep-alive' \
+  info=$(curl -s --fail "$URL/api/user" \
+  -H "Referer: $URL/" "${COMMON_HEADERS[@]}" \
   -H "Cookie: sessionId=${sessionId}; loginKey=${loginKey}" \
-  -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' \
-  -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' -H 'Priority: u=4' \
-  -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' \
   --data-raw "{\"contract_id\":\"${contract_id}\"}" | jq 2> /dev/null)
   
   # --- Вывод данных ---
@@ -78,19 +94,13 @@ function session() {
   login=$(echo "$info" | jq -r '.login')
   echo "login: $login"
   balance=$(echo "$info" | jq -r '.balance')
-  echo -e "balance: \033[35m$balance\033[0m\n"
+  echo -e "balance: ${magenta}${balance}${norm}\n"
   
   # --- Второй POST запрос данных в json-формате ---
 
-  tariff_price=$(curl -s --fail "$URL/api/services/getServices" -A "User-Agent: $USER_AGENT" \
-  -H 'Accept: */*' -H 'Accept-Language: ru,en-US;q=0.7,en;q=0.3' \
-  -H 'Accept-Encoding: gzip, deflate, br, zstd' \
-  -H "Referer: $URL/" -H 'Content-Type: application/json' \
-  -H "Origin: $URL" -H 'Connection: keep-alive' \
+  tariff_price=$(curl -s --fail "$URL/api/services/getServices" \
+  -H "Referer: $URL/" "${COMMON_HEADERS[@]}" \
   -H "Cookie: sessionId=${sessionId}; loginKey=${loginKey}" \
-  -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' \
-  -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' \
-  -H 'Priority: u=4' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' \
   --data-raw "{\"contract_id\":\"${contract_id}\"}" | jq -r ".[]" 2> /dev/null)
   
   # --- Вывод данных ---
@@ -98,27 +108,22 @@ function session() {
   tariff=$(echo $tariff_price | jq -r '.tariff')
   price=$(echo $tariff_price | jq -r '.price')
   echo "tariff: $tariff"
-  echo -e "price: \033[36m$price\033[0m"
+  echo -e "price: ${cyan}${price}${norm}"
   
   # --- Вывод истории оплат ---
 
-  if [ "$#" -gt 0 ]; then
+  if [[ "$#" -gt 0 ]]; then
     days=$(( 14 * 24 * 60 * 60 ))  # 14 дней * 24 часа * 60 минут * 60 секунд
     now_date=$(date +%Y-%m-%d)  # Текущая дата
     target_date=$(date -d "@$(( $(date +%s) - days ))" +%Y-%m-%d)  # Дата на 2 недели назад
 
-    getHistory=$(curl -s "$URL/api/payments/getHistory" -A "User-Agent: $USER_AGENT" \
-    -H 'Accept: */*' -H 'Accept-Language: ru,en-US;q=0.7,en;q=0.3' \
-    -H 'Accept-Encoding: gzip, deflate, br, zstd' -H "Referer: $URL/finance" \
-    -H 'Content-Type: application/json' -H "Origin: $URL" -H 'Connection: keep-alive' \
+    getHistory=$(curl -s --fail "$URL/api/payments/getHistory" \
+    -H "Referer: $URL/finance" "${COMMON_HEADERS[@]}" \
     -H "Cookie: sessionId=${sessionId}; loginKey=${loginKey}; GEO_CITY_ID=8527; "\
     "GEO_CITY_CODE=komsomolsknaamure; BXMOD_AUTH_LAST_PAGE_Y=%2F" \
-    -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' \
-    -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' -H 'Priority: u=4' \
-    -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' \
     --data-raw "{\"contract_id\":\"${contract_id}\",\"start_date\":\"${target_date}\",\"end_date\":\"${now_date}\"}")
     
-    echo -e "\n\033[36mИстория оплаты:\033[0m"
+    echo -e "\n${cyan}История оплаты:${norm}"
     
     echo "$getHistory" | jq -c '.[]' | while read -r item; do
       date=$(jq -r '.date' <<< "$item")
@@ -128,13 +133,11 @@ function session() {
 
       # Цвет по знаку суммы
       if [[ $amount == -* ]]; then
-        color="\033[1;31m"     # красный
-      else color="\033[1;32m"  # зелёный
+        color="${red}"
+      else color="${green}"
       fi
 
-      reset="\033[0m"  # Сброс цвета
-
-      echo -e "$date | $type | ${color}${amount}${reset} | $name"  # Печать
+      echo -e "$date | $type | ${color}${amount}${norm} | $name"  # Печать
     done
 
   fi
