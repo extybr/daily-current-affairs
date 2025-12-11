c849a5a79a3d48dd9147622da36ccc7d54da160c
Andrey <kms.amur@gmail.com>
2025-12-11 14:03:05 +1000

    update

--- scripts/open_ports.sh
+++ scripts/open_ports.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # $> ./open_ports.sh
-# Открытые порты
+# Открытые порты, слушащие порты, счетчик использования портов
 
-echo "*****  open ports on my host  *****"
+open_ports() {
+  echo "*****  открытые порты на хосте  *****"
+  netstat -lpan 2>/dev/null | \
+  awk '/^(tcp|udp)/ {print $4}' | \
+  awk -F: '{print $NF}' | \
+  sort -nu | \
+  nl
+}
+  
+listen_ports() {
+  echo "*****  слушащие порты  *****"
+  netstat -lpan 2>/dev/null | \
+  awk '$6 == "LISTEN" && /^(tcp|udp)/ {print $4}' | \
+  awk -F: '{print $NF}' | \
+  sort -nu | \
+  nl
+}
 
-# FIXME: возможные варианты >
-# lsof -i -n -P | tr -s " " | cut -d " " -f 9 | cut -d ":" -f 2 | \
-# cut -d "-" -f 1 | grep -v "NAME"
+counter_ports() {
+  echo "*****  счетчик использования портов  *****"
+  netstat -lpan 2>/dev/null | \
+  awk '/^(tcp|udp)/ {split($4, a, ":"); port = a[length(a)]; count[port]++} 
+       END {for (p in count) printf "%5d %s\n", p, count[p]}' | \
+  sort -n
+}
 
-netstat -tulpan 2> /dev/null | tr -s " " | \
-  cut -d " " -f 4 | cut -d ":" -f 2-999 | \
-  tail -n +3 | sed "s/:1://g ; s/://g" | \
-  sort -n | uniq | nl
-
+counter_ports
+listen_ports
+open_ports
