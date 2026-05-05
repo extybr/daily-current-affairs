f15cf13931000ebbd3c5867de9e2b50fccfadd0b
Andrey <kms.amur@gmail.com>
2026-05-05 23:28:02 +1000

    update

--- video/ffprobe_data.sh
+++ video/ffprobe_data.sh
@@ -9,6 +9,7 @@ term() {
 }
 
 zen() {
+  # ffprobe -pretty "$1"
   # ffprobe -v quiet -print_format json -show_format -show_streams "$1" | jq '.'
   # ffprobe -v quiet -show_format -show_streams "$1"
   # ffprobe -v error -show_entries stream=codec_name,width,height,r_frame_rate,bitrate -show_entries format=bitrate "$1"
@@ -18,5 +19,15 @@ zen() {
   zenity --info --title="$1" --text="$result"
 }
 
+exf() {
+  info=$(exiftool "$1" | grep -E \
+  "File Size|File Permissions|File Type  |^MIME Type|Doc Type|^Duration  |\
+Video Codec ID|Video Frame Rate|Image Width|Image Height|Display Width|Display Height|\
+Audio Codec ID|Audio Sample Rate|Audio Channels|Track Language  |Image Size|Megapixels|\
+Sample Rate|Channel Mode|Audio Bitrate")
+  zenity --info --title="$1" --text="$info" --width=500 --height=400
+}
+
 # term "$1"
-zen "$1"
+# zen "$1"
+exf "$1"
